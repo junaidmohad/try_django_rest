856760962dabcb92568a4a956a893046b5589f7b
junaidmohad <junaidms2020@gmail.com>
2023-08-19 18:59:17 +0530

    added the update functionality to the views

--- Web-Dev---KJSCE/backend/project/user_handler/forms.py
+++ Web-Dev---KJSCE/backend/project/user_handler/forms.py
@@ -12,4 +12,35 @@ class AccountAuthenticationForm(forms.ModelForm):
 
     class Meta: 
         model = CustomUser
-        fields = ('email')
+        fields = ('email', 'password')
+
+    def clean(self):
+        if self.is_valid():
+            email = self.cleaned_data['email']
+            password = self.cleaned_data['password']
+            if not authenticate(email=email, password=password):
+                raise forms.ValidationError("Invalid login.")
+
+#this form function is responsible for updating the details of the respective user
+class AccountUpdateForm(forms.ModelForm):
+    class Meta:
+        model = CustomUser
+        fields = ('email', 'username')
+
+    def clean_email(self):
+        if self.is_valid():
+            email = self.cleaned_data['email']
+            try:
+                customuser = CustomUser.objects.exclude(pk=self.instance.pk).get(email=email)       #here instance means to get the primary key of the user
+            except CustomUser.DoesNotExist:
+                return email
+            raise forms.ValidationError('Email "%s" is already in use.' % customuser.email)
+        
+    def clean_username(self):
+        if self.is_valid():
+            username = self.cleaned_data['username']
+            try:
+                customuser = CustomUser.objects.exclude(pk=self.instance.pk).get(username.username)
+            except CustomUser.DoesNotExist:
+                return username
+            raise forms.ValidationError('Username "%s" is already in use.' % customuser.username)
--- Web-Dev---KJSCE/backend/project/user_handler/views.py
+++ Web-Dev---KJSCE/backend/project/user_handler/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
+from user_handler.forms import AccountAuthenticationForm, AccountUpdateForm
 # from django.forms import 
 
 from . import models
@@ -52,3 +53,50 @@ class studentViewSet(viewsets.ModelViewSet):
 def logout_view(request):
     logout(request)
     return redirect('home')
+
+#this view function inflates the login form that we just created
+def login_view(request):
+    context = {}
+    user =  request.user
+    if user.is_authenticated:
+        return redirect('home')
+    
+    if request.POST:
+        form = AccountAuthenticationForm(request.POST)
+        if form.is_valid():
+            email = request.POST['email']
+            password = request.POST['password']
+            user = authenticate(email=email, password=password)
+
+            if user:
+                login(request, user)
+                return redirect('home')
+        else:
+            form = AccountAuthenticationForm()
+
+    context['login_form'] = form
+    return render(request, 'user_handler/login.html', context)              #we need to change this render function as per the frontend naming for login page
+
+
+#this view function is reponsible for updating the details of the respective user
+def account_view(request):
+    if not request.user.is_authenticated:
+        return redirect("login")
+    
+    context = {}
+
+    if request.POST:
+        form = AccountUpdateForm(request.POST, instance=request.user)               #the instance used here is to look for the primary key of the user that is authenticated. this instance refernces back to the instance in the forms.py
+        if form.is_valid():
+            form.save()
+
+    else:
+        form = AccountUpdateForm(
+            initial={
+                "email": request.user.email,
+                "username": request.user.username
+            }
+        )
+
+    context['account_form'] = form
+    return render(request, 'account/account.html', context)             #we need to change this too to update page as per the project repo
